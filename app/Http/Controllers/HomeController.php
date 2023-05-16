9ae9a582e01c9f8eeb5385db58388991151183be
olepychev <oleksandrpychev@gmail.com>
2023-05-16 12:29:47 -0700

    check mypost

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1839,9 +1839,6 @@ class HomeController extends Controller
 		}
 	}
 
-	
-
-	return response()->json(['status' => 201, 'data' => count($userpermission) ]);
 	$postdata = TblChat::join('tbl_user_rights', 'tbl_user_rights.user_id', 'tbl_chat.user_id')->where([
 								['tbl_chat.user_id', '=', $userid],
 								['chat_status', '=', 0],])
