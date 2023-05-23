8978c3cff94c01d770a1635b82743a6d33c1e486
olepychev <oleksandrpychev@gmail.com>
2023-05-22 17:47:07 -0700

    fixed also land with search text

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -156,10 +156,6 @@ class HomeController extends Controller
 	if($chat_room_id == null)
 	{
 			 
-		return response()->json([
-			'status' => 201,
-			'data' => 'aaaaaaaa',
-			 ], 201);
 
 		$total_list =  TblChat::where([
 							['chat_status', '=', '0'],
