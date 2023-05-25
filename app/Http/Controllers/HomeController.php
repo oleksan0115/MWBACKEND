85f907406e70df86e2e5aa3d42fec0debc9595aa
olepychev <oleksandrpychev@gmail.com>
2023-05-25 13:41:05 -0700

    checking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3915,7 +3915,7 @@ $sql = "( SELECT  tbl_users_taged.user_id as user_id ,tbl_users_taged.chat_id as
 		$searchtext=str_replace(" ","%",$searchtext);
 		// $total_list =  User::where([['user_name', 'LIKE', $searchtext. '%'],])->select('user_id as id','user_name as value','image')->take(30)->get();
 
-		$total_list = DB::select(DB::raw("SELECT user_id as id, user_name as value, image FROM tbl_user WHERE user_name LIKE :searchtext LIMIT 30"), ['searchtext' => $searchtext . '%']);
+		$total_list = DB::select(DB::raw("SELECT user_id as id, user_name as value, image FROM tbl_user WHERE user_name LIKE '". $searchtext . "%' ". "LIMIT 30"));
 		// $results = DB::select('SELECT * FROM users WHERE id = :id', ['id' => 1]);
 
 		// $total_list = 
